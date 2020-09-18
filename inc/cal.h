d1a42f5f2f3aca95d9a627240417dec3d93a8752
99002581 <71481629+99002581@users.noreply.github.com>
2020-09-18 23:07:33 +0530

    Add files via upload

---
+++ inc/cal.h
@@ -0,0 +1,14 @@
+#ifndef CAL_H
+#define CAL_H
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<stdlib.h>
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void modulus();
+void power();
+int factorial();
+#endif
